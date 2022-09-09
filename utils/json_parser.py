79d165fb0d31c5d1e52bf0b25a828190b6d35fe5
Ahrr Jay <rowanjohnas@gmail.com>
2022-09-08 19:44:33 -0700

    More pylint cleanup

--- utils/json_parser.py
+++ utils/json_parser.py
@@ -12,10 +12,6 @@ class JsonParser:
     """
 
     def __init__(self, json_path):
-        """
-        Init method of JsonParser
-        :param json_path: Path to local json file that gets set as a class variable
-        """
         self.json_path = os.path.join(dir_global.DATA_FILES_PATH, json_path)
 
     def read_from_json(self):
